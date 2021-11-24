1e01dd4d75e5ca22d47c5b7c604ad7252b5573d1
kyattonippu <93709139+kyattonippu@users.noreply.github.com>
2021-11-24 21:05:06 +0800

    Initial commit

---
+++ src/test/java/mayya/siv/SelenideSearchTest.java
@@ -0,0 +1,39 @@
+package mayya.siv;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideSearchTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1440x900";
+}
+
+    @Test
+
+    void checkSoftAssertionsPage() {
+
+//        Открыть страницу Selenide в Github
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+
+//        Перейти в раздел Wiki проекта
+        $("#wiki-tab").click();
+
+//        Убедиться, что в списке страниц (Pages) есть страница SoftAssertions и открытье ее
+        $(".js-wiki-more-pages-link").click();
+        $("#wiki-pages-box").$(withText("SoftAssertions")).click();
+
+//        Проверить что внутри есть пример кода для JUnit5
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class"));
+
+        sleep(5000);
+    }
+}
